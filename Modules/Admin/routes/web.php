ef88ac82920537c6cfc6141af19ea2006044770f
shobhakumari <shobha.kumari@futureprofilez.com>
2024-02-15 22:47:05 +0530

    merage

--- Modules/Admin/routes/web.php
+++ Modules/Admin/routes/web.php
@@ -29,6 +29,6 @@ Route::group(['prefix' => 'admin'], function()
     Route::post('subscription-store',[SubscriptionController::class,'store'])->name('Subscription.store');
     
 
-    
+    fefe
     
 });
